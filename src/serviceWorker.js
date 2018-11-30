0d7852650da50b3731fda3f1d3494102eefa08da
ivanazuber <ivanas.zuber@gmail.com>
2018-11-30 10:20:57 +0100

    adds no console rule to eslint configuration

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -35,7 +35,7 @@ function registerValidSW(swUrl, config) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
+              console.info(
                 'New content is available and will be used when all '
                 + 'tabs for this page are closed. See http://bit.ly/CRA-PWA.',
               );
@@ -48,7 +48,7 @@ function registerValidSW(swUrl, config) {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log('Content is cached for offline use.');
+              console.info('Content is cached for offline use.');
 
               // Execute callback
               if (config && config.onSuccess) {
