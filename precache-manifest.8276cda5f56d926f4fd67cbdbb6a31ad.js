20706a4bf300ffa8578890fd32c1b32ee1aa33a5
ivanazuber <ivanas.zuber@gmail.com>
2018-11-30 15:27:39 +0100

    Updates

---
+++ precache-manifest.8276cda5f56d926f4fd67cbdbb6a31ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b0dbe93374cc006f920",
+    "url": "/bufferapp/shared-components/static/js/runtime~main.8b0dbe93.js"
+  },
+  {
+    "revision": "2f3150f8b0ee04f04c11",
+    "url": "/bufferapp/shared-components/static/js/main.2f3150f8.chunk.js"
+  },
+  {
+    "revision": "fa67a933a7982e5ef0c2",
+    "url": "/bufferapp/shared-components/static/js/1.fa67a933.chunk.js"
+  },
+  {
+    "revision": "2f3150f8b0ee04f04c11",
+    "url": "/bufferapp/shared-components/static/css/main.7348074f.chunk.css"
+  },
+  {
+    "revision": "fa67a933a7982e5ef0c2",
+    "url": "/bufferapp/shared-components/static/css/1.61febef6.chunk.css"
+  },
+  {
+    "revision": "bc3afbfc876447bf41c735d64a4660df",
+    "url": "/bufferapp/shared-components/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/bufferapp/shared-components/precache-manifest.8276cda5f56d926f4fd67cbdbb6a31ad.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/bufferapp/shared-components/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
