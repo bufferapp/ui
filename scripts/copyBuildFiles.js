9936fd7120c6b25cdc925d67b9e822b6649cdebc
ivanazuber <ivanas.zuber@gmail.com>
2018-11-30 13:29:11 +0100

    adds a script for coping over packages to the root folder for short imports

---
+++ scripts/copyBuildFiles.js
@@ -0,0 +1,87 @@
+const path = require('path');
+const fse = require('fs-extra');
+
+const files = [
+  'README.md',
+];
+
+
+function resolveBuildPath(file) {
+  return path.resolve(__dirname, '../lib/', path.basename(file));
+}
+
+function copyFile(file) {
+  const libPath = resolveBuildPath(file);
+  return new Promise((resolve) => {
+    fse.copy(
+      file,
+      libPath,
+      (err) => {
+        if (err) throw err;
+        resolve();
+      },
+    );
+  })
+    .then(() => console.info(`Copied ${file} to ${libPath}`));
+}
+
+function createPackageFile() {
+  return new Promise((resolve) => {
+    // read from package.json
+    fse.readFile(path.resolve(__dirname, '../package.json'), 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      resolve(data);
+    });
+  })
+    .then(data => JSON.parse(data))
+    .then((packageData) => {
+      // store these properties in variables
+      const {
+        author,
+        version,
+        description,
+        keywords,
+        repository,
+        licence,
+        bugs,
+        homepage,
+        peerDependencies,
+        dependencies,
+      } = packageData;
+
+
+      const minimalPackage = {
+        name: '@bufferapp/components-new',
+        author,
+        version,
+        description,
+        main: './index.js',
+        keywords,
+        repository,
+        licence,
+        bugs,
+        homepage,
+        peerDependencies,
+        dependencies,
+      };
+
+      return new Promise((resolve) => {
+        const libPath = path.resolve(__dirname, '../lib/package.json');
+        const data = JSON.stringify(minimalPackage, null, 2);
+        // Write to a new package.json file
+        fse.writeFile(libPath, data, (err) => {
+          if (err) throw (err);
+          console.info(`Create package.json in ${libPath}`);
+          resolve();
+        });
+      });
+    });
+}
+
+Promise.all(
+  files.map(file => copyFile(file)),
+)
+  .then(() => createPackageFile());
