b0dbcc31e65ff62ec0c57b85b26d4f1f743f4bc0
Hamish Macpherson <hamstu@gmail.com>
2018-12-20 09:16:50 -0800

    tweak props table design

--- src/documentation/app/layout/content/components/Props.jsx
+++ src/documentation/app/layout/content/components/Props.jsx
@@ -1,48 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import style from 'styled-components';
+import styled from 'styled-components';
 
-const Table = style.table`
-    width: 100%;
+const Table = styled.table`
+  width: 100%;
+  border-radius: 7px;
+  border: 1px solid #e6ecf1;
+  overflow: hidden;
 `;
 
-const Label = style.th`
-    text-align: left;
-    padding: 10px 0px;
-    padding-right: 25px;
+const TableHeader = styled.thead`
+  background: #f6f7f9;
+  padding: 0 10px;
 `;
 
-const Row = style.tr`
-    padding: 10px 0px;
+const Label = styled.th`
+  text-align: left;
+  padding: 10px 0 10px 10px;
+  padding-right: 25px;
+  font-family: "Fira Code", source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+  font-weight: 500;
+  font-size: 12px;
+  opacity: 0.8;
 `;
 
-const Item = style.td`
-    padding: 10px 0px;
-    padding-right: 25px;
-    font-size: 14px;
+const Row = styled.tr`
+  padding: 5px 0px;
+`;
+
+const Item = styled.td`
+  border-top: 2px solid #e6ecf1;
+  padding: 10px 0 10px 10px;
+  padding-right: 25px;
+  font-size: 12px;
+  font-family: "Fira Code", source-code-pro, Menlo, Monaco, Consolas, "Courier New", monospace;
+  ${props => (props.bold ? 'font-weight: 500;' : '')}
 `;
 
 /** Component props table */
 const Props = ({ props }) => (
-  <Table>
-    <tbody>
+  <Table cellPadding="0" cellSpacing="0">
+    <TableHeader>
       <tr>
-        <Label>Name</Label>
-        <Label>Description</Label>
+        <Label>Property</Label>
         <Label>Type</Label>
-        <Label>Default</Label>
         <Label>Required</Label>
+        <Label>Default</Label>
+        <Label>Description</Label>
       </tr>
-    </tbody>
+    </TableHeader>
     <tbody>
       {
         Object.keys(props).map(key => (
           <Row key={key}>
-            <Item>{key}</Item>
+            <Item bold>{key}</Item>
+            <Item bold>{props[key].type.name}</Item>
+            <Item>{props[key].required ? '✓' : ''}</Item>
+            <Item>{props[key].defaultValue && props[key].defaultValue.value !== 'undefined' && props[key].defaultValue.value}</Item>
             <Item>{props[key].description}</Item>
-            <Item>{props[key].type.name}</Item>
-            <Item>{props[key].defaultValue && props[key].defaultValue.value}</Item>
-            <Item>{props[key].required && 'X'}</Item>
           </Row>
         ))
       }
