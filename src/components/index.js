df98580c94d3b34cf014ca069724586906c6630d
Sol Vm <38150554+ssvmvss@users.noreply.github.com>
2019-02-22 18:16:05 -0300

    Export new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 export { default as Button } from './Button';
 export { default as Select } from './Select';
-export { default as ButtonSelect } from './ButtonSelect';
 export { default as AppShell } from './AppShell';
 export { default as Text } from './Text';
+export { default as Form } from './Form';
+export { default as FormBlock } from './FormBlock';
+export { default as Sidebar } from './Sidebar';
+export { default as Card } from './Card';
