eae00fbc983f946a54f89a264bc6706ca6d054c2
Sol Vm <38150554+ssvmvss@users.noreply.github.com>
2019-02-22 18:15:47 -0300

    Add extra font related styles

--- src/components/style/fonts.js
+++ src/components/style/fonts.js
@@ -1,7 +1,7 @@
+// Font families
 export const fontFamily = '"Roboto", sans-serif';
 
-export const fontSize = '14px';
-
+// Font weights
 export const fontWeightThin = 100;
 export const fontWeightExtraLight = 200;
 export const fontWeightLight = 300;
@@ -10,4 +10,11 @@ export const fontWeightMedium = 500;
 export const fontWeightSemiBold = 600;
 export const fontWeightBold = 700;
 
+// Line heights
 export const lineHeight = 1.5;
+
+// Font sizes
+export const fontSize   = '14px';
+export const fontSizeH1 = '32px';
+export const fontSizeH2 = '24px';
+export const fontSizeH3 = '18px';
