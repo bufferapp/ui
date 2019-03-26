df93b5c0e6d4c2b8534bf2b720d152d3f5c95312
James Morris <me@jamesmorr.is>
2019-03-26 14:52:17 +0000

    New nav bar

--- src/documentation/app/layout/navbar/NavBar.jsx
+++ src/documentation/app/layout/navbar/NavBar.jsx
@@ -2,103 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  display: block;
-  margin: 0px;
-  padding: 0px;
-`;
-
-const Top = styled.div`
-  background: #fff;
+const Container = styled.div`
+  z-index: 999;
+  background: #3d3d3d;
   border-bottom: 1px solid #d4dadf;
-  box-shadow: 0 1px 1px 0 rgba(116, 129, 141, 0.1);
-  z-index: 20;
-  display: flex;
-  align-items: stretch;
-  position: relative;
-  flex: 0 80px;
-  min-height: 80px;
-  padding-left: 24px;
-  padding-right: 24px;
-`;
-
-const Middle = styled.div`
-  max-width: 1400px;
-  width: 100%;
-  margin: 0px auto;
+  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2);
   display: flex;
   align-items: center;
-  padding: 0;
-  flex: auto;
+  justify-content: space-between;
 `;
 
-const Title = styled.a`
-  cursor: pointer;
-  display: flex;
-  text-decoration: none;
-  align-items: center;
-`;
-
-const Logo = styled.div`
-  display: block;
+const Logo = styled.h1`
+  width: 167px;
+  height: 64px;
   margin: 0px;
   padding: 0px;
-`;
-
-const Image = styled.img`
-  max-width: 100%;
-  border-radius: 3px;
+  background: transparent url('/images/logo.png') no-repeat top left;
+  background-size: 100%;
+  text-indent: -9999em;
   overflow: hidden;
-  width: 40px;
-  height: 40px;
-  font-size: 24px;
-  line-height: 1.5;
-  font-weight: 500;
 `;
 
-const TextWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 0px;
-  padding: 0px;
-  padding-left: 16px;
-  color: #242a31;
-`;
-
-const Text = styled.h1`
-  font-size: 28px;
-  line-height: 1.5;
-  font-weight: 500;
-`;
-
-const VersionText = styled.h2`
+const Version = styled.div`
   font-size: 14px;
-  line-height: 1.5;
-  font-weight: 500;
-  margin-left: 20px;
+  font-weight: 700;
+  color: #FFFFFF;
+  margin: 0 1rem 0 0;
 `;
 
 /** Top Navigation */
 const NavBar = ({ title, version }) => (
-  <Wrapper>
-    <Top>
-      <Middle>
-        <Title>
-          <Logo>
-            <Image src="https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/spaces%2F-LFNym8ScnaWKWBQFWTw%2Favatar.png?generation=1529427935328806&alt=media" />
-          </Logo>
-          <TextWrapper>
-            <Text>{title}</Text>
-            <VersionText>
-              {' '}
-              v.
-              {version}
-            </VersionText>
-          </TextWrapper>
-        </Title>
-      </Middle>
-    </Top>
-  </Wrapper>
+  <Container>
+    <Logo>
+      {title}
+    </Logo>
+    <Version>
+      v.
+      {version}
+    </Version>
+  </Container>
 );
 
 NavBar.propTypes = {
