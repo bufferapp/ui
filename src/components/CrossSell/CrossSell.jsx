d2c7550ebdc242992ab819c25b4276b75a783264
Juliana Gomez <juliana@buffer.com>
2019-06-04 16:21:56 -0500

    v1 of the crossSell component

--- src/components/CrossSell/CrossSell.jsx
+++ src/components/CrossSell/CrossSell.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 import PropTypes from 'prop-types';
-import { CrossSellStyled, CrossSellHeader, CardsContainer } from './style';
+import {
+  CrossSellStyled,
+  CrossSellHeader,
+  CardsContainer,
+  ButtonContainer,
+  Footer,
+  Card,
+  CustomH3,
+  ButtonMargin,
+  SVGContainer,
+} from './style';
 import CrossSellContent from './CrossSellContent/CrossSellContent';
 import Text from '../Text/Text';
 import Button from '../Button/Button';
@@ -27,19 +37,19 @@ export default class CrossSell extends React.Component {
     const { product } = this.props;
     const cards = [
       {
-        image: 'http://placekitten.com/167/259',
+        image: 'http://placekitten.com/259/167',
         title: 'Single Team Inbox',
         description:
           'Reply to all your Instagram, Facebook and Twitter messages in one collaborative team inbox - DMs, comments, mentions and more. ',
       },
       {
-        image: 'http://placekitten.com/167/259',
+        image: 'http://placekitten.com/259/167',
         title: 'Single Team Inbox',
         description:
           'Reply to all your Instagram, Facebook and Twitter messages in one collaborative team inbox - DMs, comments, mentions and more. ',
       },
       {
-        image: 'http://placekitten.com/167/259',
+        image: 'http://placekitten.com/259/167',
         title: 'Single Team Inbox',
         description:
           'Reply to all your Instagram, Facebook and Twitter messages in one collaborative team inbox - DMs, comments, mentions and more. ',
@@ -50,11 +60,11 @@ export default class CrossSell extends React.Component {
       return (
         <CardsContainer>
           {cards.map(card => (
-            <div>
+            <Card>
               <img src={card.image} alt="kitty" />
-              <div>{card.title}</div>
-              <div>{card.description}</div>
-            </div>
+              <CustomH3>{card.title}</CustomH3>
+              <Text type="p">{card.description}</Text>
+            </Card>
           ))}
         </CardsContainer>
       );
@@ -66,22 +76,41 @@ export default class CrossSell extends React.Component {
 
     if (product === 'reply') {
       return (
-        <div>
-          <Button
-            type="primary"
-            onClick={() => {}}
-            label="Click Me"
-            fullWidth
-          />
-
-          <Button
-            type="secondary"
-            onClick={() => {}}
-            label="Click Me"
-            fullWidth
-          />
+        <Footer>
+          <ButtonContainer>
+            <ButtonMargin>
+              <Button
+                type="primary"
+                size="large"
+                onClick={() => {}}
+                label="Start a 14-Day Free Trial"
+                fullWidth
+              />
+            </ButtonMargin>
+            <ButtonMargin>
+              <Button
+                type="secondary"
+                size="large"
+                onClick={() => {}}
+                label="Learn More About Buffer Reply"
+                fullWidth
+              />
+            </ButtonMargin>
+          </ButtonContainer>
           <Text type="p">Pricing starts at $50/mo. Cancel anytime.</Text>
-        </div>
+          <SVGContainer>
+            <svg
+              viewBox="0 0 1280 165"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1364.93 523L1419 364.723C1419 364.723 1304.97 -127.978 974.074 31.9313C779.673 125.878 530.916 100.499 389.5 65.6508C95.8639 -6.70832 -53.8721 165.309 -174.355 199.539C-294.838 233.769 -335 101.25 -335 101.25V523H1364.93Z"
+                fill="white"
+              />
+            </svg>
+          </SVGContainer>
+        </Footer>
       );
     }
   };
--- src/components/CrossSell/style.js
+++ src/components/CrossSell/style.js
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const CrossSellStyled = styled.div`
   height: 100vh;
   width: 100vw;
+  background-color: #e5e5e5;
+  z-index: 1;
 `;
 
 export const CrossSellHeader = styled.div`
@@ -10,8 +12,55 @@ export const CrossSellHeader = styled.div`
   flex-direction: column;
   align-items: center;
   max-width: 582px;
+  margin-top: 40px;
+  text-align: center;
 `;
 
 export const CardsContainer = styled.div`
   display: flex;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Footer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Card = styled.div`
+  width: 291px;
+  height: 328px;
+  background: #ffffff;
+  box-shadow: 0px 5px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  padding: 16px;
+  margin: 32px 16px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CustomH3 = styled.h3`
+  font-family: Roboto;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 18px;
+  line-height: 28px;
+  margin-bottom: 8px;
+`;
+
+export const ButtonMargin = styled.div`
+  margin: 0 8px 16px;
+  width: 258px;
+`;
+
+export const SVGContainer = styled.div`
+  width: 100%;
+  position: fixed;
+  bottom: 0;
 `;
