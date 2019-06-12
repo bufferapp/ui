235bd40c34efda20fae53276bcb9b0b5598741ca
Sol <38150554+ssvmvss@users.noreply.github.com>
2019-06-12 16:06:04 -0300

    Allow scroll on cross-sell page

--- src/components/CrossSell/style.js
+++ src/components/CrossSell/style.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
 export const CrossSellStyled = styled.div`
-  height: 100vh;
   width: 100vw;
   background-color: #f5f5f5;
   display: flex;
   flex-direction: column;
   align-items: center;
+  overflow: scroll;
 `;
 
 export const CrossSellHeader = styled.div`
