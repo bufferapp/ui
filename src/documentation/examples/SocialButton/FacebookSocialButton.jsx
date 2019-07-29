665a1e03df862932575eb10a7a7c87fb64675905
Ana Almeida <anafilipadealmeida@gmail.com>
2019-07-29 13:41:44 +0100

    Add Social Button component to documentation

---
+++ src/documentation/examples/SocialButton/FacebookSocialButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SocialButton from '@bufferapp/ui/SocialButton';
+
+/** Facebook Social Button */
+export default function ExampleSocialButton() {
+  return (
+    <div style={{ position: 'relative' }}>
+      <SocialButton channel="facebook" />
+    </div>
+  );
+}
