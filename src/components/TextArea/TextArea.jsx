3a0539acfeeaf74da4b3f0c94b80fe2a5d5431ee
Juliana Gomez <juliana@gomezjuliana.com>
2019-09-06 16:11:56 -0500

    add initial and focus states

--- src/components/TextArea/TextArea.jsx
+++ src/components/TextArea/TextArea.jsx
@@ -2,20 +2,59 @@ import React from 'react';
 
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import * as Styles from './style';
+import Text from '../Text'
+// mport * as Styles from './style';
 
-const TextAreaStyled = styled.div`
-  ${Styles.base}
+const Container = styled.div`
+  
 `;
 
-const TextArea = ({ children }) => (
-  <TextAreaStyled>
-    {children}
-  </TextAreaStyled>
+const StyledTextArea = styled.textarea`
+  background: #FFFFFF;
+  border: 1px solid #B8B8B8;
+  box-sizing: border-box;
+  border-radius: 4px;
+  margin-top: 8px;
+  resize: none;
+  :focus {
+    border: 3px solid #ABB7FF;
+border-radius: 4px;
+  }
+`
+
+const StyledLabel = styled.label`
+  display: flex;
+  flex-direction: column;
+`
+
+const TextArea = ({ children, label, ...props }) => (
+  <Container>
+    <StyledLabel htmlFor="story">
+      <Text
+        htmlFor="foo"
+        type='label'
+      >
+        {label}
+      </Text>
+      <StyledTextArea
+        id="story"
+        {...props}
+      >
+        {children}
+      </StyledTextArea>
+    </StyledLabel>
+  </Container>
 );
 
 TextArea.propTypes = {
   children: PropTypes.node.isRequired,
+  label: PropTypes.string.isRequired,
+  placeholder: PropTypes.string
 };
+
+TextArea.defaultProps = {
+  placeholder: undefined,
+}
+
 
 export default TextArea;
--- src/documentation/examples/TextArea/TextArea.jsx
+++ src/documentation/examples/TextArea/TextArea.jsx
@@ -4,7 +4,7 @@ import TextArea from '@bufferapp/ui/TextArea';
 /** TextArea Example */
 export default function ExampleTextArea() {
   return (
-    <TextArea>
+    <TextArea label='Textarea (Normal)' placeholder='ghosts and stuff' rows="30">
       Hello, world!
     </TextArea>
   );
