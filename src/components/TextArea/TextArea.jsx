f84d0306aa275c74cb0f8f4c49867d3e9ea5962f
Juliana Gomez <juliana@gomezjuliana.com>
2019-10-02 14:41:00 -0500

    allow forward ref on textarea component

--- src/components/TextArea/TextArea.jsx
+++ src/components/TextArea/TextArea.jsx
@@ -1,40 +1,56 @@
 import React from 'react';
-
 import PropTypes from 'prop-types';
-
 import Text from '../Text';
 import {HelpTextWrapper, HelpText} from '../Input/style';
 import { Warning } from '../Icon';
 import {Container, StyledTextArea} from './style';
 
-const TextArea = ({value, label, hasError, help, disabled, rows, onChange, id, fullHeight, ...props }) => (
-  <Container>
-    <Text
-      htmlFor={id}
-      type='label'
-    >
-      {label}
-    </Text>
-    <StyledTextArea
-      id={id}
-      {...props}
-      hasError={hasError}
-      disabled={disabled}
-      rows={rows > 20 ? 20 : rows}
-      value={value}
-      onChange={onChange}
-      fullHeight={fullHeight}
-    />
-    {hasError && (
-      <HelpTextWrapper>
-        <Warning size="medium" />
-        <HelpText type="help" htmlFor={id} hasError={hasError}>
-          {help}
-        </HelpText>
-      </HelpTextWrapper>
-    )}
-  </Container>
-);
+export default class TextArea extends React.Component {
+  render() {
+    const {
+      value, 
+      label, 
+      hasError, 
+      help, 
+      disabled, 
+      rows, 
+      onChange, 
+      id, 
+      fullHeight,
+      forwardRef, 
+      ...props 
+    } = this.props;
+    return (
+      <Container>
+        <Text
+          htmlFor={id}
+          type='label'
+        >
+          {label}
+        </Text>
+        <StyledTextArea
+          id={id}
+          {...props}
+          hasError={hasError}
+          disabled={disabled}
+          rows={rows > 20 ? 20 : rows}
+          value={value}
+          onChange={onChange}
+          fullHeight={fullHeight}
+          ref={forwardRef}
+        />
+        {hasError && (
+        <HelpTextWrapper>
+          <Warning size="medium" />
+          <HelpText type="help" htmlFor={id} hasError={hasError}>
+            {help}
+          </HelpText>
+        </HelpTextWrapper>
+      )}
+      </Container>
+)
+  }
+}
 
 TextArea.propTypes = {
   /** It adds a label on top of the textarea box. */
@@ -52,7 +68,12 @@ TextArea.propTypes = {
   /** The id to link the textarea with the label */
   id: PropTypes.string.isRequired,
   /** If the textarea should take the height of the parent div */
-  fullHeight: PropTypes.bool
+  fullHeight: PropTypes.bool,
+  /** 
+   * this consumed by the default export that is wrapping the component into a ForwardRef
+   * @ignore
+   */
+  forwardRef: PropTypes.node,
 };
 
 TextArea.defaultProps = {
@@ -60,7 +81,6 @@ TextArea.defaultProps = {
   hasError: false,
   disabled: false,
   rows: 4,
-  fullHeight: false
+  fullHeight: false,
+  forwardRef: undefined,
 }
-
-export default TextArea;
