12076704b9e91a96d9a32a4bc657b5f98096b1a7
AL <amyleeannebennett@gmail.com>
2019-10-24 15:51:09 -0500

    Add open icon

---
+++ src/documentation/examples/Icon/OpenNew.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import OpenNewIcon from '@bufferapp/ui/Icon/Icons/OpenNew';
+
+/** OpenNew */
+export default function OpenNewIconExample() {
+  return (
+    <OpenNewIcon size="large" />
+  );
+}
