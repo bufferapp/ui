0e4c839151c2d7ab1473c26d28c8ce96bf1fdeb7
Hamish Macpherson <hamstu@gmail.com>
2019-10-29 14:53:00 -0700

    Updates

---
+++ precache-manifest.6d9632aa915874e97b4a9909fb17cbbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c1892a644dfb4daba0f",
+    "url": "/ui/static/js/runtime~main.0c1892a6.js"
+  },
+  {
+    "revision": "fcfca6b59044c2be162e",
+    "url": "/ui/static/js/main.fcfca6b5.chunk.js"
+  },
+  {
+    "revision": "e4286fda0613b755bb64",
+    "url": "/ui/static/js/1.e4286fda.chunk.js"
+  },
+  {
+    "revision": "fcfca6b59044c2be162e",
+    "url": "/ui/static/css/main.cb019678.chunk.css"
+  },
+  {
+    "revision": "e4286fda0613b755bb64",
+    "url": "/ui/static/css/1.db97756b.chunk.css"
+  },
+  {
+    "revision": "6d544d4398cf1fbeac125439c1fa3b07",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.9abae953af002f494c3ec4f99110d726.js"
+  "/ui/precache-manifest.6d9632aa915874e97b4a9909fb17cbbb.js"
 );
 
 workbox.clientsClaim();
