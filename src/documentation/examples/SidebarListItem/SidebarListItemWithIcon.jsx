5eb26e4a5a5eac1324ddd312eaec19610efd7fc8
Ana Almeida <anafilipadealmeida@gmail.com>
2020-01-08 16:38:58 +0000

    Add new SidebarListItem example

---
+++ src/documentation/examples/SidebarListItem/SidebarListItemWithIcon.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SidebarListItem from '@bufferapp/ui/SidebarListItem';
+import { Warning } from '@bufferapp/ui/Icon';
+
+/** SidebarListItem With Badge Icon Example */
+export default function ExampleSidebarListItem() {
+  return (
+    <SidebarListItem
+      id="1a"
+      title="Label Default"
+      onItemClick={() => console.info('hey')}
+      badges={123}
+      badgeIcon={<Warning color="grayDarker" />}
+    />
+  );
+}
