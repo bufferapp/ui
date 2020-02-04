db0a74bdacc8f460e72b69e61ad2181f331c379f
Federico Weber <federicoweber@gmail.com>
2020-02-03 17:20:34 -0800

    Updates

---
+++ precache-manifest.2360f0c8453962fee952aa1669610bdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4911f8b88d2627424b92",
+    "url": "/ui/static/css/main.8b4a993f.chunk.css"
+  },
+  {
+    "revision": "4911f8b88d2627424b92",
+    "url": "/ui/static/js/main.4911f8b8.chunk.js"
+  },
+  {
+    "revision": "be6a4b7845b63d4af870",
+    "url": "/ui/static/css/1.76f5eece.chunk.css"
+  },
+  {
+    "revision": "be6a4b7845b63d4af870",
+    "url": "/ui/static/js/1.be6a4b78.chunk.js"
+  },
+  {
+    "revision": "0c1892a644dfb4daba0f",
+    "url": "/ui/static/js/runtime~main.0c1892a6.js"
+  },
+  {
+    "revision": "40ca25d00b0acdb5411ee19f7c4519a7",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.454181e8d34ced0b5172e3db94e201bc.js"
+  "/ui/precache-manifest.2360f0c8453962fee952aa1669610bdf.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/ui/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
