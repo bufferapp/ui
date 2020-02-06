80cb62d5c266d4aeb49f12533905a8b305efb684
Juliana Gomez <juliana@gomezjuliana.com>
2020-02-06 15:29:11 -0500

    Updates

---
+++ precache-manifest.ee80cf88102cfb4cd17a8664e6de6938.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c1892a644dfb4daba0f",
+    "url": "/ui/static/js/runtime~main.0c1892a6.js"
+  },
+  {
+    "revision": "592aceffe59003cff324",
+    "url": "/ui/static/js/main.592aceff.chunk.js"
+  },
+  {
+    "revision": "d142c1501e2935a5ed9d",
+    "url": "/ui/static/js/1.d142c150.chunk.js"
+  },
+  {
+    "revision": "592aceffe59003cff324",
+    "url": "/ui/static/css/main.889711dd.chunk.css"
+  },
+  {
+    "revision": "d142c1501e2935a5ed9d",
+    "url": "/ui/static/css/1.ef0a8ccc.chunk.css"
+  },
+  {
+    "revision": "4ab7231e4e198527bc57cf4dc8e1a053",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.2360f0c8453962fee952aa1669610bdf.js"
+  "/ui/precache-manifest.ee80cf88102cfb4cd17a8664e6de6938.js"
 );
 
 workbox.clientsClaim();
