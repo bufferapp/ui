7049a6efda765362b78ef19c7323b20b8625524f
Juliana Gomez <54291087+gmzjuliana@users.noreply.github.com>
2020-02-10 16:16:46 -0500

    rename customHTML to customLabel (#175)

--- src/documentation/examples/Tooltip/option/withCustomLabel.jsx
+++ src/documentation/examples/Tooltip/option/withCustomLabel.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Tooltip from '@bufferapp/ui/Tooltip';
 import Avatar from '@bufferapp/ui/Avatar'; // eslint-disable-line
 
-const customHTML = (
+const customLabel = (
   <div>
     <span style={{ color: '#21F32A'}}>0.4%</span>
     <p style={{ display: 'inline' }}> potential reach</p>
   </div>
 );
 
-/** With CustomHTML */
+/** With CustomLabel */
 export default function ExampleTooltip() {
   return (
-    <Tooltip customHTML={customHTML} position="bottom">
+    <Tooltip customLabel={customLabel} position="bottom">
       <Avatar
         src="https://s3.amazonaws.com/buffer-ui/Default+Avatar.png"
         alt="@joelgascoigne"
