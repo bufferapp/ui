e57814d4af9ef1ac08be6d1b2cda93e3988f465b
AL <amyleeannebennett@gmail.com>
2020-02-18 10:46:21 -0600

    Add calendar icon

---
+++ src/documentation/examples/Icon/Calendar.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import CalendarIcon from '@bufferapp/ui/Icon/Icons/Calendar';
+
+/** Calendar */
+export default function CalendarIconExample() {
+  return (
+    <CalendarIcon size="large" />
+  );
+}
