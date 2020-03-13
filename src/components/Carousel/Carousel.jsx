83843e64ba180b0e9432e0595d5226499e2caffd
Juliana Gomez <juliana@gomezjuliana.com>
2020-03-13 16:39:34 -0500

    add PR comments

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -4,6 +4,7 @@ import styled, { css } from 'styled-components';
 import Button from '../Button/index';
 import ArrowRight from '../Icon/Icons/ArrowRight';
 import ArrowLeft from '../Icon/Icons/ArrowLeft';
+import CarouselItems from './CarouselItems';
 import { gray, white } from '../style/colors';
 
 const CarouselStyled = styled.div`
@@ -41,17 +42,12 @@ const MainList = styled.ul`
   transition: left 0.5s ease-in;
 `;
 
-const MainListItem = styled.li`
-  list-style: none;
-`;
-
 // grabbed these styles from https://a11yproject.com/posts/how-to-hide-content/
 const Announcement = styled.div`
   position: absolute !important;
   height: 1px;
   width: 1px;
   overflow: hidden;
-  clip: rect(1px 1px 1px 1px); /* IE6, IE7 */
   clip: rect(1px, 1px, 1px, 1px);
   white-space: nowrap; /* added line */
 `;
@@ -91,46 +87,6 @@ class Carousel extends React.Component {
     currentSlideIndex: 0,
   };
 
-  moveBackward = () => {
-    const { left } = this.state;
-    const { width } = this.props;
-    if (left === 0) {
-      // if it's the first item we still want to update the currentSlideIndex so we can
-      // update the aria-hidden attribute appropriately
-      this.setState(prevState => ({
-        currentSlideIndex:
-          prevState.currentSlideIndex === 0
-            ? prevState.currentSlideIndex
-            : prevState.currentSlideIndex - 1,
-      }));
-      return;
-    }
-    this.setState(prevState => ({
-      left: prevState.left + parseInt(width, 10),
-      currentSlideIndex: prevState.currentSlideIndex - 1,
-    }));
-  };
-
-  moveForward = () => {
-    const { children, width } = this.props;
-    if (this.verifyLastItem(children.length, width)) {
-      // if it's the last item we still want to update the currentSlideIndex so we can
-      // update the aria-hidden attribute appropriately
-      this.setState(prevState => ({
-        currentSlideIndex:
-          prevState.currentSlideIndex === children.length - 1
-            ? prevState.currentSlideIndex
-            : prevState.currentSlideIndex + 1,
-      }));
-      return;
-    }
-
-    this.setState(prevState => ({
-      left: prevState.left - parseInt(width, 10),
-      currentSlideIndex: prevState.currentSlideIndex + 1,
-    }));
-  };
-
   verifyLastItem = (lengthOfChildren, widthOfEachItem) => {
     const { left } = this.state;
     const finalLength = (lengthOfChildren - 1) * parseInt(widthOfEachItem, 10);
@@ -138,35 +94,20 @@ class Carousel extends React.Component {
     return left === -finalLength;
   };
 
-  renderItems = () => {
-    const { currentSlideIndex } = this.state;
-    const { children } = this.props;
-    const items = children.map((child, index) => (
-      <MainListItem key={index} aria-hidden={currentSlideIndex !== index}>
-        {child}
-      </MainListItem>
-    ));
-
-    return items;
-  };
-
-  renderAnnouncement = () => {
-    const { currentSlideIndex } = this.state;
-    const { children } = this.props;
-    const content = (
-      <Announcement aria-live="polite" aria-atomic="true" tabIndex={-1}>
-        Slide
-        {currentSlideIndex + 1}
-        out of
-        {children.length}
-      </Announcement>
-    );
-
-    return content;
-  };
-
   goToSlide = index => {
-    const { width } = this.props;
+    const { width, children } = this.props;
+    const { currentSlideIndex, left } = this.state;
+
+    // going backwards
+    if (currentSlideIndex > index) {
+      if (left === 0) return;
+    }
+
+    // going forwards
+    if (currentSlideIndex < index) {
+      if (this.verifyLastItem(React.Children.count(children), width)) return;
+    }
+
     const newLeft = parseInt(width, 10) * index;
     this.setState({
       left: -newLeft,
@@ -179,24 +120,34 @@ class Carousel extends React.Component {
     const { children, width } = this.props;
     return (
       <CarouselStyled>
-        {this.renderAnnouncement()}
+        {/* this announcement is hidden but notifies screen reader users when the slide has changed */}
+        <Announcement aria-live="polite" aria-atomic="true" tabIndex={-1}>
+          Slide
+          {currentSlideIndex + 1}
+          out of
+          {children.length}
+        </Announcement>
         <ButtonOverlapContainer left>
           <Button
             type="secondary"
             icon={<ArrowLeft />}
             hasIconOnly
             onClick={() => {
-              this.moveBackward();
+              this.goToSlide(currentSlideIndex - 1);
             }}
             label="Backwards"
           />
         </ButtonOverlapContainer>
         <Content>
           <Window width={width}>
-            <MainList left={left}>{this.renderItems()}</MainList>
+            <MainList left={left}>
+              <CarouselItems currentSlideIndex={currentSlideIndex}>
+                {children}
+              </CarouselItems>
+            </MainList>
           </Window>
           <IndicatorList>
-            {children.map((child, index) => (
+            {React.Children.map(children, (child, index) => (
               <IndicatorListItem key={index}>
                 <IndicatorButton
                   type="button"
@@ -213,7 +164,7 @@ class Carousel extends React.Component {
             icon={<ArrowRight />}
             hasIconOnly
             onClick={() => {
-              this.moveForward();
+              this.goToSlide(currentSlideIndex + 1);
             }}
             label="Forwards"
           />
@@ -223,14 +174,9 @@ class Carousel extends React.Component {
   }
 }
 
-Carousel.defaultProps = {
-  children: [],
-  width: '200px',
-};
-
 Carousel.propTypes = {
   /** The content within the carousel */
-  children: PropTypes.array.isRequired,
+  children: PropTypes.node.isRequired,
 
   /** The normalized width for each item */
   width: PropTypes.string.isRequired,
