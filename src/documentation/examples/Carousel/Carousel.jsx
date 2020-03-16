a4ade9e07886e58ec2e8e4409575763ba5be9e5d
Juliana Gomez <54291087+gmzjuliana@users.noreply.github.com>
2020-03-16 14:10:23 -0500

    add nicer images to exmaples (#182)

--- src/documentation/examples/Carousel/Carousel.jsx
+++ src/documentation/examples/Carousel/Carousel.jsx
@@ -5,9 +5,21 @@ import Carousel from '@bufferapp/ui/Carousel';
 export default function ExampleCarousel() {
   return (
     <Carousel width="400px">
-      <img src="http://placekitten.com/400/300" alt="" />
-      <img src="http://placekitten.com/400/300" alt="" />
-      <img src="http://placekitten.com/400/300" alt="" />
+      <img
+        src="https://buffer-analyze.s3.amazonaws.com/images/modal-pro-bg.png"
+        alt="slide 1"
+        width="400"
+      />
+      <img
+        src="https://buffer-analyze.s3.amazonaws.com/images/modal-pro-bg.png"
+        alt="slide 2"
+        width="400"
+      />
+      <img
+        src="https://buffer-analyze.s3.amazonaws.com/images/modal-pro-bg.png"
+        alt="slide 3"
+        width="400"
+      />
     </Carousel>
   );
 }
