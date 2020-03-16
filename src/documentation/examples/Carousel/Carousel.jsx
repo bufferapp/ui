e41239a1817867bafe1b11716cbc4fe671de03ca
Juliana Gomez <54291087+gmzjuliana@users.noreply.github.com>
2020-03-16 13:58:02 -0500

    Carousel (#181)
    * add basic functionality
    
    * create example with carousel inside of the modal
    
    * add tweaks
    
    * polish carousel
    
    * add PR comments
    
    * update tests
    
    * actually update tests
    
    * add indicator button hidden text
    
    * update snapshot tests

---
+++ src/documentation/examples/Carousel/Carousel.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Carousel from '@bufferapp/ui/Carousel';
+
+/** Carousel Example */
+export default function ExampleCarousel() {
+  return (
+    <Carousel width="400px">
+      <img src="http://placekitten.com/400/300" alt="" />
+      <img src="http://placekitten.com/400/300" alt="" />
+      <img src="http://placekitten.com/400/300" alt="" />
+    </Carousel>
+  );
+}
---
+++ src/documentation/examples/Modal/with-carousel.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import Modal from '@bufferapp/ui/Modal';
+import Button from '@bufferapp/ui/Button';
+import Carousel from '@bufferapp/ui/Carousel';
+
+/** With Carousel Modal */
+export default function ModalTest() {
+  const [modalOpen, openModal] = useState(false);
+  return (
+    <div style={{ width: '100%', height: '400px', position: 'relative' }}>
+      <Button
+        type="primary"
+        onClick={() => {
+          openModal(true);
+        }}
+        label="Bring the modal!"
+      />
+      {modalOpen && (
+        <Modal
+          action={{ label: 'Don’t Panic!', callback: () => openModal(false) }}
+          wide
+        >
+          <div>
+            <Carousel width="200px">
+              <img src="http://placekitten.com/200/300" alt="" />
+              <img src="http://placekitten.com/200/300" alt="" />
+              <img src="http://placekitten.com/200/300" alt="" />
+            </Carousel>
+          </div>
+        </Modal>
+      )}
+    </div>
+  );
+}
