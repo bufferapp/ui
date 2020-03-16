b92250133379a2d1c0f6dde2b943e00f3d9062e0
Juliana Gomez <juliana@gomezjuliana.com>
2020-03-16 14:07:48 -0500

    add nicer images to exmaples

--- src/documentation/examples/Modal/with-carousel.jsx
+++ src/documentation/examples/Modal/with-carousel.jsx
@@ -21,10 +21,22 @@ export default function ModalTest() {
           wide
         >
           <div>
-            <Carousel width="200px">
-              <img src="http://placekitten.com/200/300" alt="" />
-              <img src="http://placekitten.com/200/300" alt="" />
-              <img src="http://placekitten.com/200/300" alt="" />
+            <Carousel width="400px">
+              <img
+                src="https://buffer-analyze.s3.amazonaws.com/images/modal-pro-bg.png"
+                alt="slide 1"
+                width="400"
+              />
+              <img
+                src="https://buffer-analyze.s3.amazonaws.com/images/modal-pro-bg.png"
+                alt="slide 2"
+                width="400"
+              />
+              <img
+                src="https://buffer-analyze.s3.amazonaws.com/images/modal-pro-bg.png"
+                alt="slide 3"
+                width="400"
+              />
             </Carousel>
           </div>
         </Modal>
