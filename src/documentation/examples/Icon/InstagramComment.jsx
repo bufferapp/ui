0bd3453672b7e6733a9e807e538c9a418623e840
Juliana Gomez <54291087+gmzjuliana@users.noreply.github.com>
2020-03-19 18:44:44 -0500

    add new icons - Carousel, IGComment, IGLike, Movie (#184)
    * add new icons - Carousel, IGComment, IGLike, Movie
    
    * update changelog

---
+++ src/documentation/examples/Icon/InstagramComment.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import InstagramCommentIcon from '@bufferapp/ui/Icon/Icons/InstagramComment';
+
+/** InstagramComment */
+export default function InstagramCommentIconExample() {
+  return (
+    <InstagramCommentIcon size="large" />
+  );
+}
---
+++ src/documentation/examples/Icon/InstagramLike.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import InstagramLikeIcon from '@bufferapp/ui/Icon/Icons/InstagramLike';
+
+/** InstagramLike */
+export default function InstagramLikeIconExample() {
+  return (
+    <InstagramLikeIcon size="large" />
+  );
+}
