1eda859bb0d23bb15f5e1b220664c992afc747e7
Sol <38150554+ssvmvss@users.noreply.github.com>
2020-03-26 18:39:16 -0300

    Add example for AppShell with limited products

---
+++ src/documentation/examples/AppShell/WithLimitedProducts.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import AppShell from '@bufferapp/ui/AppShell';
+import {
+  Gear,
+} from '@bufferapp/ui/Icon';
+
+import { gray } from '@bufferapp/ui/style/colors';
+
+const userMenuItems = [
+  {
+    id: '2',
+    title: 'Preferences',
+    icon: <Gear color={gray} />,
+    onItemClick: () => { console.info('Preferences') },
+  },
+];
+
+const helpMenuItems = [
+  {
+    id: '1',
+    title: 'FAQ',
+    onItemClick: () => {},
+  },
+  {
+    id: '2',
+    title: 'Status',
+    onItemClick: () => {},
+  },
+  {
+    id: '3',
+    title: 'Pricing & Plans',
+    onItemClick: () => {},
+  },
+  {
+    id: '4',
+    title: 'Wishlist',
+    onItemClick: () => {},
+  },
+];
+
+/** AppShell with limited products */
+export default function ExampleAppShell() {
+  return (
+    <AppShell
+      products={['publish', 'analyze']}
+      activeProduct="publish"
+      user={{
+        name: 'Hamish Macpherson',
+        email: 'hamstu@gmail.com',
+        avatar: 'https://pbs.twimg.com/profile_images/847849987841167360/WEVTxvUA_400x400.jpg',
+        menuItems: userMenuItems,
+      }}
+      helpMenuItems={helpMenuItems}
+      content={<div>Main content.</div>}
+    />
+  );
+}
