ac000a7d4a9af212e9570d310cd7427b0288cdfe
Sol <38150554+ssvmvss@users.noreply.github.com>
2020-06-12 11:30:40 -0300

    Add new icons

--- src/components/Icon/Icons/Negative.jsx
+++ src/components/Icon/Icons/Negative.jsx
@@ -9,7 +9,7 @@ import createIconComponent from '../utils/createIconComponent';
 const NegativeIcon = createIconComponent({
   content: (
     <g>
-      <path fillRule="evenodd" clipRule="evenodd" d="M0 4a4 4 0 0 1 4-4h8a4 4 0 0 1 4 4v8a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4V4zm7 0a1 1 0 0 1 2 0v5a1 1 0 1 1-2 0V4zm1 7a1 1 0 1 0 0 2 1 1 0 0 0 0-2z" />
+      <path d="M6.5 2a1.5 1.5 0 1 1 3 0v7.5a1.5 1.5 0 0 1-3 0V2zM6.5 14a1.5 1.5 0 1 1 3 0 1.5 1.5 0 0 1-3 0z" />
     </g>
   ),
 });
