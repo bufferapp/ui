05804dc904d0e04b0f241a01fe65d3d0af1e6944
Juliana Gomez <54291087+gmzjuliana@users.noreply.github.com>
2020-06-23 16:35:33 -0500

    new icons: add, organic, percentageup, percentagedown, promoted (#218)

---
+++ src/documentation/examples/Icon/Organic.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import OrganicIcon from '@bufferapp/ui/Icon/Icons/Organic';
+
+/** Organic */
+export default function OrganicIconExample() {
+  return (
+    <OrganicIcon size="large" />
+  );
+}
---
+++ src/documentation/examples/Icon/PercentageUp.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import PercentageUpIcon from '@bufferapp/ui/Icon/Icons/PercentageUp';
+
+/** PercentageUp */
+export default function PercentageUpIconExample() {
+  return (
+    <PercentageUpIcon size="large" />
+  );
+}
