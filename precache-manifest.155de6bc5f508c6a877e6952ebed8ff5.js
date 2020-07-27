a882f88038948177bd8ec78bf69f615ba1e6c727
AL <amyleeannebennett@gmail.com>
2020-07-27 11:06:22 -0500

    Updates

---
+++ precache-manifest.155de6bc5f508c6a877e6952ebed8ff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "084103ab80df361b475f",
+    "url": "/ui/static/js/runtime~main.084103ab.js"
+  },
+  {
+    "revision": "3cf418cb2f5fd9016cc0",
+    "url": "/ui/static/js/main.3cf418cb.chunk.js"
+  },
+  {
+    "revision": "6464cac3ea99045caa7897dd056d9ca5",
+    "url": "/ui/static/js/2.caca58fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caca58fa3d6d89e142b2",
+    "url": "/ui/static/js/2.caca58fa.chunk.js"
+  },
+  {
+    "revision": "3cf418cb2f5fd9016cc0",
+    "url": "/ui/static/css/main.e2f17014.chunk.css"
+  },
+  {
+    "revision": "caca58fa3d6d89e142b2",
+    "url": "/ui/static/css/2.46b88ebb.chunk.css"
+  },
+  {
+    "revision": "08022301bb9afa919a4f732ba29abd4a",
+    "url": "/ui/index.html"
+  }
+];
