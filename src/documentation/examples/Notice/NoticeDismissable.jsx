e53efa3aade6e32942ee13f166ceeac18e2d01b5
Juliana Gomez <juliana@gomezjuliana.com>
2020-08-13 14:49:14 -0500

    fix notice note message

--- src/documentation/examples/Notice/NoticeDismissable.jsx
+++ src/documentation/examples/Notice/NoticeDismissable.jsx
@@ -6,7 +6,8 @@ export default function ExampleNotice() {
   return (
     // eslint-disable-next-line 
     <Notice dismiss={() => console.log('dismissed!')} type="note">
-      We&apos;re aware of an issue and we&apos;re actively working to resolve the situation
+      We&apos;re actively working on improving this feature and would love your
+      feedback!
     </Notice>
   );
 }
