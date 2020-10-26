06f089d4754d700ba26a038c1d6a3e1838bc8fe1
Dace <dacewillmott@gmail.com>
2020-10-26 11:55:28 -0400

    Add documentation for danger styles

---
+++ src/documentation/examples/Button/Type/TypeDanger.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from '@bufferapp/ui/Button';
+
+/** Danger */
+export default function ExampleTypeDanger() {
+  return <Button type="danger" onClick={() => {}} label="Click Me" />;
+}
