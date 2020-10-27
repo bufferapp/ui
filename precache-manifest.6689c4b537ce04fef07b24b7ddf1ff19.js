227c8192cbd00da83ae06b9fa20793aeec62f11d
Ana Almeida <anafilipadealmeida@gmail.com>
2020-10-27 14:52:06 +0000

    Updates

---
+++ precache-manifest.6689c4b537ce04fef07b24b7ddf1ff19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c591ff4dd58ba263b66c",
+    "url": "/ui/static/css/main.292191a9.chunk.css"
+  },
+  {
+    "revision": "c591ff4dd58ba263b66c",
+    "url": "/ui/static/js/main.c591ff4d.chunk.js"
+  },
+  {
+    "revision": "45a14f7cca06b831b2ad",
+    "url": "/ui/static/js/runtime~main.45a14f7c.js"
+  },
+  {
+    "revision": "05891a55d67ec8c72b8c",
+    "url": "/ui/static/css/2.70ef7c88.chunk.css"
+  },
+  {
+    "revision": "05891a55d67ec8c72b8c",
+    "url": "/ui/static/js/2.05891a55.chunk.js"
+  },
+  {
+    "revision": "39ba5efc9c6e122a415fa68bab23a1fb",
+    "url": "/ui/static/js/2.05891a55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d18c50336ca7e40fd0b7faebab82408d",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.e7547663d99feb264400aa99dd5ed574.js"
+  "/ui/precache-manifest.6689c4b537ce04fef07b24b7ddf1ff19.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/ui/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
