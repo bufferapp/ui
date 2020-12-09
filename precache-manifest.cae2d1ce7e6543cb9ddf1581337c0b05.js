8d6e81852f4737f6ff0f6b3f5eb5232b0bf56edc
María A Uribe <mariale.uribe@gmail.com>
2020-12-09 10:07:12 +0100

    Updates

---
+++ precache-manifest.cae2d1ce7e6543cb9ddf1581337c0b05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "45a14f7cca06b831b2ad",
+    "url": "/ui/static/js/runtime~main.45a14f7c.js"
+  },
+  {
+    "revision": "31dafbe9f277eff934d5",
+    "url": "/ui/static/js/main.31dafbe9.chunk.js"
+  },
+  {
+    "revision": "39ba5efc9c6e122a415fa68bab23a1fb",
+    "url": "/ui/static/js/2.bcffcbde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcffcbde95c2d648b3a6",
+    "url": "/ui/static/js/2.bcffcbde.chunk.js"
+  },
+  {
+    "revision": "31dafbe9f277eff934d5",
+    "url": "/ui/static/css/main.2233e0dd.chunk.css"
+  },
+  {
+    "revision": "bcffcbde95c2d648b3a6",
+    "url": "/ui/static/css/2.512b4e6d.chunk.css"
+  },
+  {
+    "revision": "9f6bc1ea641afd94b7b501a029ffb5f8",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.a0a6d05439b965bd1a95f7abbf5cd499.js"
+  "/ui/precache-manifest.cae2d1ce7e6543cb9ddf1581337c0b05.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/ui/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
