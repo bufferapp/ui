57810dde618a0f7b4d3f6014300f3f3cb0ed131d
Juliana Gomez <juliana@gomezjuliana.com>
2021-01-05 10:08:22 -0500

    Updates

---
+++ precache-manifest.aaf1bef39e6590e2f1260f035d60effe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "084103ab80df361b475f",
+    "url": "/ui/static/js/runtime~main.084103ab.js"
+  },
+  {
+    "revision": "ca429f9388b38f31dbf4",
+    "url": "/ui/static/js/main.ca429f93.chunk.js"
+  },
+  {
+    "revision": "6464cac3ea99045caa7897dd056d9ca5",
+    "url": "/ui/static/js/2.dbac56ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbac56ae12d6b7b96d98",
+    "url": "/ui/static/js/2.dbac56ae.chunk.js"
+  },
+  {
+    "revision": "ca429f9388b38f31dbf4",
+    "url": "/ui/static/css/main.c64d5f43.chunk.css"
+  },
+  {
+    "revision": "dbac56ae12d6b7b96d98",
+    "url": "/ui/static/css/2.38177fb7.chunk.css"
+  },
+  {
+    "revision": "979ea8551581f5cb48e7a841986bcf79",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.cae2d1ce7e6543cb9ddf1581337c0b05.js"
+  "/ui/precache-manifest.aaf1bef39e6590e2f1260f035d60effe.js"
 );
 
 workbox.clientsClaim();
