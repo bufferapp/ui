04648e452ec3b2f9b056a3e4dda676bcbd45bd37
Juliana Gomez <54291087+gmzjuliana@users.noreply.github.com>
2021-01-21 15:35:15 -0500

    Update src/components/Carousel/Carousel.jsx
    Co-authored-by: Ana Filipa de Almeida <anafilipadealmeida@gmail.com>

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -49,7 +49,7 @@ const Announcement = styled.div`
   height: 1px;
   width: 1px;
   overflow: hidden;
-  clip: rect(1px, 1px, 1px, 1px);
+  clip-path: inset(1px 1px 1px 1px);
   white-space: nowrap; /* added line */
   margin: 0;
 `;
