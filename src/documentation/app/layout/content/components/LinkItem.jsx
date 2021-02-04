b78b627b02066e5fb3842dd682aa7d461e177b34
Bill Fienberg <billfienberg@gmail.com>
2021-02-04 02:37:02 -0600

    Extract linkWrapperStyleString into a variable for reuse

--- src/documentation/app/layout/content/components/LinkItem.jsx
+++ src/documentation/app/layout/content/components/LinkItem.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const ExternalLinkWrapper = styled.a`
+const linkWrapperStyleString = `
     position: relative;
     display: flex;
     flex-direction: row;
@@ -26,31 +26,14 @@ const ExternalLinkWrapper = styled.a`
     &:hover {
        border-color: #2E5DF8;
     }
+`
+
+const ExternalLinkWrapper = styled.a`
+    ${linkWrapperStyleString}
 `;
 
 const LinkWrapper = styled(Link)`
-    position: relative;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-self: stretch;
-    align-self: stretch;
-    color: rgb(36, 42, 49);
-    background-color: rgb(255, 255, 255);
-    box-shadow: rgba(116, 129, 141, 0.1) 0px 3px 8px 0px;
-    margin: 0px;
-    padding: 0px;
-    border-radius: 3px;
-    border-width: 1px;
-    border-style: solid;
-    border-color: rgb(230, 236, 241);
-    border-image: initial;
-    transition: border 200ms ease 0s;
-    text-decoration: none;
-    cursor: pointer;
-    &:hover {
-       border-color: #2E5DF8;
-    }
+    ${linkWrapperStyleString}
 `;
 
 const LinkBody = styled.div`
