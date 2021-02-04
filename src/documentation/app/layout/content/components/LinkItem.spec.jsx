a797be3fad01d17dddfc507090a7ea998031e205
Bill Fienberg <billfienberg@gmail.com>
2021-02-04 03:44:53 -0600

    Add snapshot test for LinkItem documentation component

---
+++ src/documentation/app/layout/content/components/LinkItem.spec.jsx
@@ -0,0 +1,7 @@
+/* eslint-disable react/jsx-filename-extension */
+import snap from 'jest-auto-snapshots';
+import 'jest-styled-components';
+
+import LinkItem from './LinkItem';
+
+snap(LinkItem, './LinkItem.jsx');
