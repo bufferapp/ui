ebfb1682f0ca583825c3a6e7be9bf48cdec2bd2b
Carlos Muñoz <cmunozgar@gmail.com>
2021-07-27 07:45:45 +0200

    Add 3 new icons: Canva, Pin, Title

---
+++ src/documentation/examples/Icon/Title.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import TitleIcon from '@bufferapp/ui/Icon/Icons/Title';
+
+/** Title */
+export default function TitleIconExample() {
+  return (
+    <TitleIcon size="large" />
+  );
+}
