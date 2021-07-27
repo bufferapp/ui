467b4b7e54ff2d06e111e9825fb8df5fbb4acdba
Carlos Muñoz <cmunozgar@gmail.com>
2021-07-27 07:33:44 +0200

    new tag examples

---
+++ src/documentation/examples/Tag/IconTag.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tag from '@bufferapp/ui/Tag';
+import FlashIcon from '@bufferapp/ui/Icon/Icons/Flash';
+
+/** Tag with icon */
+export default function IconTag() {
+  return (
+    <Tag color="purpleLighter">
+      <FlashIcon color="purple" />
+    </Tag>
+  );
+}
