9b5dd0591cf09d5255c3721637429806bc6562ec
Carlos Muñoz <cmunozgar@gmail.com>
2021-07-27 07:33:37 +0200

    improve styles

--- src/components/Tag/style.js
+++ src/components/Tag/style.js
@@ -1,12 +1,13 @@
 import styled from 'styled-components';
-import { fontSizeSmall, fontWeightMedium } from '../style/fonts';
+import { fontSize, fontWeightMedium, lineHeight } from '../style/fonts';
 import textColors, * as Colors from '../style/colors';
 
 export const Tag = styled.span`
   display: inline-block;
   padding: 4px 8px;
   font-weight: ${fontWeightMedium};
-  font-size: ${fontSizeSmall};
+  font-size: ${fontSize};
+  line-height: ${lineHeight};
   margin-left: 8px;
   border-radius: 100px;
   color: ${(props) => textColors[props.textColor]};
