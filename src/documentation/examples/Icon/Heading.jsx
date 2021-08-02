bd37e9e3d42677a0f668ded98453f201a0f6cdc4
Carlos Muñoz <cmunozgar@gmail.com>
2021-08-02 05:54:24 +0200

    Added five new icons

---
+++ src/documentation/examples/Icon/Heading.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import HeadingIcon from '@bufferapp/ui/Icon/Icons/Heading';
+
+/** Heading */
+export default function HeadingIconExample() {
+  return (
+    <HeadingIcon size="large" />
+  );
+}
---
+++ src/documentation/examples/Icon/Subheading.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import SubheadingIcon from '@bufferapp/ui/Icon/Icons/Subheading';
+
+/** Subheading */
+export default function SubheadingIconExample() {
+  return (
+    <SubheadingIcon size="large" />
+  );
+}
