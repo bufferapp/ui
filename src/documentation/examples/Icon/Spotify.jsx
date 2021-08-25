0b8736fdcef6eaaf807c22376ae0dad7f4598140
Juliana Gomez <54291087+gmzjuliana@users.noreply.github.com>
2021-08-25 11:52:58 -0500

    Update icons (#322)
    * add new icons
    
    * rename snap icon to snapchat
    
    * update changelog

---
+++ src/documentation/examples/Icon/Spotify.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import SpotifyIcon from '@bufferapp/ui/Icon/Icons/Spotify';
+
+/** Spotify */
+export default function SpotifyIconExample() {
+  return (
+    <SpotifyIcon size="large" />
+  );
+}
---
+++ src/documentation/examples/Icon/Tiktok.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import TiktokIcon from '@bufferapp/ui/Icon/Icons/Tiktok';
+
+/** Tiktok */
+export default function TiktokIconExample() {
+  return (
+    <TiktokIcon size="large" />
+  );
+}
