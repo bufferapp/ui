5ae2ddfc345e9a68db23d42efcf58ce918165fc3
guldenmw <guldenmw@gmail.com>
2021-09-21 10:00:41 +0200

    Don't allow Search component to be controlled as mixed controlled and uncontrolled class components generally rely on anti-patterns with odd side-effects

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -5,21 +5,8 @@ import { SearchInput, SearchWrapper } from './style';
 /** Search input that filters the Select items and adds keyboard navigation */
 export default class Search extends React.Component {
   state = {
-    controlled: false,
     search: '',
   };
-
-  static getDerivedStateFromProps = (props, state) => {
-    // Allows controlling input. If value is not passed,
-    // state will be managed internally.
-    if (props.value !== state.value) {
-      return {
-        ...state,
-        controlled: true,
-        search: props.value
-      }
-    }
-  }
 
   componentDidUpdate(prevProps) {
     if(prevProps.isOpen !== this.props.isOpen){
@@ -27,33 +14,25 @@ export default class Search extends React.Component {
     }
   }
 
+  updateSearch = (search) => {
+    this.setState({
+      search,
+    });
+  }
 
   onChange = (event) => {
     const { onChange } = this.props;
-    const { controlled } = this.state;
-
     const search = event.target.value;
 
     onChange(search);
-    // Only update state if not controlled to save re-renders
-    if (!controlled) {
-      this.setState({
-        search,
-      });
-    }
+    this.updateSearch(search);
   }
 
   clearSearch = () => {
     const { onChange } = this.props;
-    const { controlled } = this.state;
 
     onChange('');
-    // Only update state if not controlled to save re-renders
-    if (!controlled) {
-      this.setState({
-        search: '',
-      });
-    }
+    this.updateSearch('');
   }
 
   render() {
@@ -61,8 +40,8 @@ export default class Search extends React.Component {
       placeholder,
       onClick,
       height,
-      clearSearchOnBlur,
-      clearSearchOnFocus,
+      resetOnBlur,
+      resetOnFocus,
     } = this.props;
 
     const { search } = this.state;
@@ -76,9 +55,8 @@ export default class Search extends React.Component {
           ref={inputRef => this.inputRef = inputRef}
           onChange={event => this.onChange(event)}
           onClick={onClick}
-          clearSearchOnBlur={clearSearchOnBlur}
-          onBlur={clearSearchOnBlur ? this.clearSearch : undefined}
-          onFocus={clearSearchOnFocus ? this.clearSearch : undefined}
+          onBlur={resetOnBlur ? this.clearSearch : undefined}
+          onFocus={resetOnFocus ? this.clearSearch : undefined}
           height={height}
         />
       </SearchWrapper>
@@ -89,9 +67,6 @@ export default class Search extends React.Component {
 export const searchPropTypes = {
   /** Search placeholder */
   placeholder: PropTypes.string,
-
-  /** The value of the input. Should only be provided if input is controlled. */
-  value: PropTypes.string,
 
   /** Function to call on search input change */
   onChange: PropTypes.func.isRequired,
@@ -106,10 +81,10 @@ export const searchPropTypes = {
   height: PropTypes.string,
 
   /** Should the search clear on blur */
-  clearSearchOnBlur: PropTypes.bool,
+  resetOnBlur: PropTypes.bool,
 
   /** Should the search clear on focus */
-  clearSearchOnFocus: PropTypes.bool,
+  resetOnFocus: PropTypes.bool,
 
   /** Is onBlur event */
   onBlur: PropTypes.func
@@ -119,11 +94,10 @@ Search.propTypes = searchPropTypes;
 
 Search.defaultProps = {
   placeholder: '',
-  value: undefined,
   height: 'tall',
   isOpen: false,
-  clearSearchOnBlur: false,
-  clearSearchOnFocus: false,
+  resetOnBlur: false,
+  resetOnFocus: false,
   onClick: () => {},
   onBlur: () => {}
 }
