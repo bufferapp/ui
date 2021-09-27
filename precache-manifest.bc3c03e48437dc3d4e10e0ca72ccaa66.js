253b34b5c0ef7381c91ae89a82255d30a17a18da
guldenmw <guldenmw@gmail.com>
2021-09-27 11:20:41 +0200

    Updates

---
+++ precache-manifest.bc3c03e48437dc3d4e10e0ca72ccaa66.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "21a57fd1cb6e2d711160",
+    "url": "/ui/static/css/main.40ed66db.chunk.css"
+  },
+  {
+    "revision": "21a57fd1cb6e2d711160",
+    "url": "/ui/static/js/main.21a57fd1.chunk.js"
+  },
+  {
+    "revision": "45a14f7cca06b831b2ad",
+    "url": "/ui/static/js/runtime~main.45a14f7c.js"
+  },
+  {
+    "revision": "566d808e8a54b2f42d6b",
+    "url": "/ui/static/css/2.4529d7cb.chunk.css"
+  },
+  {
+    "revision": "566d808e8a54b2f42d6b",
+    "url": "/ui/static/js/2.566d808e.chunk.js"
+  },
+  {
+    "revision": "39ba5efc9c6e122a415fa68bab23a1fb",
+    "url": "/ui/static/js/2.566d808e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8cdf80a8b163969e13cc2acaa08c261",
+    "url": "/ui/index.html"
+  }
+];
