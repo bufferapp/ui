b1547470f1dfebe57966b93ca1263fc10ec3a3b3
Arek Panek <arekpn@gmail.com>
2021-10-20 10:49:22 +0200

    Fix

---
+++ src/documentation/examples/Icon/Dropbox.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import DropboxIcon from '@bufferapp/ui/Icon/Icons/Dropbox';
+
+/** Dropbox */
+export default function DropboxIconExample() {
+  return (
+    <DropboxIcon size="large" />
+  );
+}
