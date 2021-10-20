304e7b0687c0635a59e2b9118150f0e90f7f82f9
Arek Panek <arekpn@gmail.com>
2021-10-20 10:36:22 +0200

    New icons for GDrive, Dropbox, Onedrive, Unsplash

---
+++ src/documentation/examples/Icon/Grid.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import GridIcon from '@bufferapp/ui/Icon/Icons/Grid';
+
+/** Grid */
+export default function GridIconExample() {
+  return (
+    <GridIcon size="large" />
+  );
+}
