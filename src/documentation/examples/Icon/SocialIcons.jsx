eb491aa3d230c91b75dd48594cd282c9ae2d2105
Sol <38150554+ssvmvss@users.noreply.github.com>
2022-02-14 15:15:07 -0300

    Update icons

---
+++ src/documentation/examples/Icon/SocialIcons.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import SocialIconsIcon from '@bufferapp/ui/Icon/Icons/SocialIcons';
+
+/** SocialIcons */
+export default function SocialIconsIconExample() {
+  return (
+    <SocialIconsIcon size="large" />
+  );
+}
