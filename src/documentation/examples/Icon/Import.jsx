9948436d250c300ba063e5f1bb94326f6463752c
Sol <38150554+ssvmvss@users.noreply.github.com>
2022-02-14 15:27:49 -0300

    Add new icons

---
+++ src/documentation/examples/Icon/Import.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import ImportIcon from '@bufferapp/ui/Icon/Icons/Import';
+
+/** Import */
+export default function ImportIconExample() {
+  return (
+    <ImportIcon size="large" />
+  );
+}
