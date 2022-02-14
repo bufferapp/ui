d70bd8749961f22497677621c9110e7436b06eb4
ssvmvss <38150554+ssvmvss@users.noreply.github.com>
2022-02-14 15:42:35 -0300

    Adds latest icons (#350)
    * Add new icons
    
    * Update CHANGELOG

---
+++ src/documentation/examples/Icon/Video.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import VideoIcon from '@bufferapp/ui/Icon/Icons/Video';
+
+/** Video */
+export default function VideoIconExample() {
+  return (
+    <VideoIcon size="large" />
+  );
+}
