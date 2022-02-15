9723e705f1789f8ccc0ee934b2a5994541b75eb2
Sol <38150554+ssvmvss@users.noreply.github.com>
2022-02-15 15:48:09 -0300

    Updates

---
+++ precache-manifest.85a2070fb736c607ba9fb3ca3b5841c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc62e37e1d3d08849274",
+    "url": "/ui/static/css/main.3600fc6a.chunk.css"
+  },
+  {
+    "revision": "bc62e37e1d3d08849274",
+    "url": "/ui/static/js/main.bc62e37e.chunk.js"
+  },
+  {
+    "revision": "798a685cd0ecb9ff5c4a",
+    "url": "/ui/static/js/runtime~main.798a685c.js"
+  },
+  {
+    "revision": "52ed9a852a0eb05e894f",
+    "url": "/ui/static/css/2.32b00be1.chunk.css"
+  },
+  {
+    "revision": "52ed9a852a0eb05e894f",
+    "url": "/ui/static/js/2.52ed9a85.chunk.js"
+  },
+  {
+    "revision": "10135ab346d3958b6f976c93d813db11",
+    "url": "/ui/static/js/2.52ed9a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d215f7ab6e3454c5402c4ad1559ede3d",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.36b1b2af126521ce276816bd3ed86499.js"
+  "/ui/precache-manifest.85a2070fb736c607ba9fb3ca3b5841c8.js"
 );
 
 workbox.clientsClaim();
