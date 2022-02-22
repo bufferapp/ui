4098241e83cf6772230bfe4d76a410b843d85591
Sol <38150554+ssvmvss@users.noreply.github.com>
2022-02-22 16:44:28 -0300

    Add 'DragIndicator' icon

---
+++ src/documentation/examples/Icon/DragIndicator.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import DragIndicatorIcon from '@bufferapp/ui/Icon/Icons/DragIndicator';
+
+/** DragIndicator */
+export default function DragIndicatorIconExample() {
+  return (
+    <DragIndicatorIcon size="large" />
+  );
+}
