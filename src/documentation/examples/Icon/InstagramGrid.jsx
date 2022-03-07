087032aeaa89083a87f609c185bcae709495ed0a
ssvmvss <38150554+ssvmvss@users.noreply.github.com>
2022-03-07 14:41:47 -0300

    Add new icons (#357)
    * Add new icons
    
    * Update CHANGELOG
    
    * Fix typo

---
+++ src/documentation/examples/Icon/InstagramGrid.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import InstagramGridIcon from '@bufferapp/ui/Icon/Icons/InstagramGrid';
+
+/** InstagramGrid */
+export default function InstagramGridIconExample() {
+  return (
+    <InstagramGridIcon size="large" />
+  );
+}
