923db562f8a4e5a4ad14b1d196d04df0648f77bb
Eduardo Esclapes <eduardo.esclapes@gmail.com>
2022-05-18 11:38:33 +0200

    Updates

---
+++ precache-manifest.267889af781d605faa9789f952326dbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "beda2fa53ab3380a2c70",
+    "url": "/ui/static/css/main.f3ce4d1f.chunk.css"
+  },
+  {
+    "revision": "beda2fa53ab3380a2c70",
+    "url": "/ui/static/js/main.beda2fa5.chunk.js"
+  },
+  {
+    "revision": "798a685cd0ecb9ff5c4a",
+    "url": "/ui/static/js/runtime~main.798a685c.js"
+  },
+  {
+    "revision": "886ee0ed112de150adcc",
+    "url": "/ui/static/css/2.1c15d934.chunk.css"
+  },
+  {
+    "revision": "886ee0ed112de150adcc",
+    "url": "/ui/static/js/2.886ee0ed.chunk.js"
+  },
+  {
+    "revision": "10135ab346d3958b6f976c93d813db11",
+    "url": "/ui/static/js/2.886ee0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af1beabf845c284672d7586fc7b943fd",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.37805265380168cbe21fd1ff6976bdd5.js"
+  "/ui/precache-manifest.267889af781d605faa9789f952326dbf.js"
 );
 
 workbox.clientsClaim();
