79378a1554a9895e6227db3f35c8b9f5879f78f1
MayaUribe <mariale.uribe@gmail.com>
2022-05-24 12:20:54 +0200

    Updates

---
+++ precache-manifest.4f10d8c6a6cbdbc53eead6163dcffd53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e2e22dfff25d3b8c628",
+    "url": "/ui/static/css/main.7b9c6eca.chunk.css"
+  },
+  {
+    "revision": "9e2e22dfff25d3b8c628",
+    "url": "/ui/static/js/main.9e2e22df.chunk.js"
+  },
+  {
+    "revision": "798a685cd0ecb9ff5c4a",
+    "url": "/ui/static/js/runtime~main.798a685c.js"
+  },
+  {
+    "revision": "b49ccba7939e9140d7ab",
+    "url": "/ui/static/css/2.0c22a9fb.chunk.css"
+  },
+  {
+    "revision": "b49ccba7939e9140d7ab",
+    "url": "/ui/static/js/2.b49ccba7.chunk.js"
+  },
+  {
+    "revision": "10135ab346d3958b6f976c93d813db11",
+    "url": "/ui/static/js/2.b49ccba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1acb7d1d216645c63790508c04725149",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.6eafc0d8fa21ffca273699c8c9eb53ee.js"
+  "/ui/precache-manifest.4f10d8c6a6cbdbc53eead6163dcffd53.js"
 );
 
 workbox.clientsClaim();
