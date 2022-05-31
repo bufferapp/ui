29f9ccc2629bab82565929c6d9d59a9ba6642ff8
Arek Panek <arekpn@gmail.com>
2022-05-31 16:51:51 +0200

    Bringing back PaywallTag icon; Gmb icon renamed to Gbp (#380)

---
+++ src/documentation/examples/Icon/Gbp.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import GbpIcon from '@bufferapp/ui/Icon/Icons/Gbp';
+
+/** Gbp */
+export default function GbpIconExample() {
+  return (
+    <GbpIcon size="large" />
+  );
+}
