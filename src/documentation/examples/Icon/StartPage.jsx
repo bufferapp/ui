2e6555f23aadda4b2c78971025098286dc8dfe9d
Arek Panek <arekpn@gmail.com>
2022-06-09 16:05:56 +0200

    Fix: Change to Gbp Icon and adding StartPage icon

---
+++ src/documentation/examples/Icon/StartPage.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import StartPageIcon from '@bufferapp/ui/Icon/Icons/StartPage';
+
+/** StartPage */
+export default function StartPageIconExample() {
+  return (
+    <StartPageIcon size="large" />
+  );
+}
