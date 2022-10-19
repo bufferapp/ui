ca616ccd795cd25d35352ae89c0e878da7f6a09e
Arek Panek <arekpn@gmail.com>
2022-10-19 08:24:06 -0400

    Updates

---
+++ precache-manifest.f6223c7e2fa83d93c18511814c8657a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c35fae1b9e2c565b55b",
+    "url": "/ui/static/css/main.a52e38cb.chunk.css"
+  },
+  {
+    "revision": "2c35fae1b9e2c565b55b",
+    "url": "/ui/static/js/main.2c35fae1.chunk.js"
+  },
+  {
+    "revision": "798a685cd0ecb9ff5c4a",
+    "url": "/ui/static/js/runtime~main.798a685c.js"
+  },
+  {
+    "revision": "5e287540694cd364028d",
+    "url": "/ui/static/css/2.902cd9ee.chunk.css"
+  },
+  {
+    "revision": "5e287540694cd364028d",
+    "url": "/ui/static/js/2.5e287540.chunk.js"
+  },
+  {
+    "revision": "10135ab346d3958b6f976c93d813db11",
+    "url": "/ui/static/js/2.5e287540.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea13947f9371ad73f8868ba45bfb716d",
+    "url": "/ui/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ui/precache-manifest.7e31aaca9ff103d4b1ac038aa6f1c9b5.js"
+  "/ui/precache-manifest.f6223c7e2fa83d93c18511814c8657a3.js"
 );
 
 workbox.clientsClaim();
