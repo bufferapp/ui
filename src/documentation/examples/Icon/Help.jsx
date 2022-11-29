fb45de3b19019a93484277a7e29cbd4003e2435f
Gisete Kindahl <gisete@buffer.com>
2022-11-29 14:36:18 +0000

    pull last icon changes

---
+++ src/documentation/examples/Icon/Help.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import HelpIcon from '@bufferapp/ui/Icon/Icons/Help';
+
+/** Help */
+export default function HelpIconExample() {
+  return (
+    <HelpIcon size="large" />
+  );
+}
