c83fce4af5e7c1588bf79e7dc44505f02d8ac632
philippemiguet <philippe.miguet@gmail.com>
2023-01-19 12:56:43 +0100

    add default type definition

---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for Buffer UI Components
+// Project: Buffer
+// Definitions by: Juliana Gomez <www.gomezjuliana.com>
+
+declare module '@bufferapp/ui/Icon'
+declare module '@bufferapp/ui/Icon/Icons/*'
+declare module '@bufferapp/ui/style/colors'
+declare module '@bufferapp/ui/Tag'
+declare module '@bufferapp/ui/Text'
+declare module '@bufferapp/ui/Input'
+declare module '@bufferapp/ui/TextArea'
+declare module '@bufferapp/ui/Button'
+declare module '@bufferapp/ui/SimpleModal'
+declare module '@bufferapp/ui/style/fonts'
+declare module '@bufferapp/ui/Notification'
+declare module '@bufferapp/ui/Select'
+declare module '@bufferapp/ui/Loader'
+declare module '@bufferapp/ui/Tooltip'
+declare module '@bufferapp/ui/Link'
