23fecfa2cf656e4aee7675ecdf4c63c8dd655724
Carlos Muñoz <cmunozgar@gmail.com>
2023-02-02 09:59:12 +0100

    Updates

---
+++ precache-manifest.a8056e008f47fe05653f20fd8d421e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f4f127a870764f8d853",
+    "url": "/ui/static/css/main.70aef580.chunk.css"
+  },
+  {
+    "revision": "8f4f127a870764f8d853",
+    "url": "/ui/static/js/main.8f4f127a.chunk.js"
+  },
+  {
+    "revision": "798a685cd0ecb9ff5c4a",
+    "url": "/ui/static/js/runtime~main.798a685c.js"
+  },
+  {
+    "revision": "152899aff6b734bc1100",
+    "url": "/ui/static/css/2.4d375b3e.chunk.css"
+  },
+  {
+    "revision": "152899aff6b734bc1100",
+    "url": "/ui/static/js/2.152899af.chunk.js"
+  },
+  {
+    "revision": "10135ab346d3958b6f976c93d813db11",
+    "url": "/ui/static/js/2.152899af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6177968205c62e257b5e93d4a7fdf84b",
+    "url": "/ui/index.html"
+  }
+];
