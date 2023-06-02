4728d28b28ba8938dd9cbf57fdc1153b6160221d
Philippe Miguet <philippe@buffer.com>
2023-06-02 12:21:25 +0200

    new publish beta script

---
+++ scripts/publishBeta.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+#if [ -z $1 ]
+#  then
+#    echo -e $RED"Publish error:" $NC "plesase provide the current package.json version"
+#    exit 1
+#fi
+
+version=$(jq -r .version package.json)
+version=$(echo "$version" | sed 's/-beta.*//')
+
+revision=$(git rev-parse --short HEAD)
+betaVersion="$version-beta.$revision"
+
+echo -e $BLUE"Publishing a new" ${betaVersion} "version"$NC
+
+yarn version --new-version $betaVersion --no-git-tag-version && \
+yarn run build:lib && \
+cd ./lib && \
+yarn publish --tag=beta
