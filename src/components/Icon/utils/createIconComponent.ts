ec16a69d53b1c66f1943323d05a993d23a10a953
Philippe Miguet <philippe@buffer.com>
2023-06-05 18:37:11 +0200

    fix ts errors

--- src/components/Icon/utils/createIconComponent.ts
+++ src/components/Icon/utils/createIconComponent.ts
@@ -1,15 +1,16 @@
 import { createElement } from 'react'
 import Icon from '../Icon'
 
-// @ts-expect-error TS(7031) FIXME: Binding element 'content' implicitly has an 'any' ... Remove this comment to see the full error message
-const createIconComponent = ({ content }) => (props) =>
-  createElement(
-    Icon,
-    {
-      ...props,
-      viewBox: '0 0 16 16',
-    },
-    content,
-  )
+const createIconComponent =
+  // @ts-expect-error TS(7031) FIXME: Binding element 'content' implicitly has an 'any' ... Remove this comment to see the full error message
+  ({ content }) => (props: any) =>
+    createElement(
+      Icon,
+      {
+        ...props,
+        viewBox: '0 0 16 16',
+      },
+      content,
+    )
 
 export default createIconComponent
