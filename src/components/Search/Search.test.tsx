06e2eb3f412091a33b03d67b03417dc4c6fadef3
Philippe Miguet <philippe@buffer.com>
2023-06-05 18:48:20 +0200

    use react v17

--- src/components/Search/Search.test.tsx
+++ src/components/Search/Search.test.tsx
@@ -2,8 +2,7 @@
 import React from 'react'
 // @ts-expect-error TS(7016) FIXME: Could not find a declaration file for module 'enzy... Remove this comment to see the full error message
 import Enzyme, { shallow } from 'enzyme'
-// @ts-expect-error TS(7016) FIXME: Could not find a declaration file for module 'enzy... Remove this comment to see the full error message
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import Search from './Search'
 
 Enzyme.configure({ adapter: new Adapter() })
